22b75e92ef076927cbea7b2621258c4e463c5d78
Sevak <sevak.martirosyan1@gmail.com>
2024-09-21 00:45:10 +1000

    "finish desktop version v0.0.2"

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,5 +1,7 @@
 import * as flsWebpFunction from './modules/webfunctions.js';
 import * as flsError from './modules/Error.js';
+import { hover } from './modules/hover.js';
 
+hover();
 flsError.funError();
 flsWebpFunction.isWebp();
